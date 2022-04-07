3677f57eae22ddb74ef4e37ee556276488097192
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2022-04-07 06:24:30 -0400

    setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="slurm_utils",
+    version="0.1",
+    author="Naoki Yokoyama",
+    author_email="naokiyokoyama@github",
+    description="Utilities for Slurm using Python",
+    url="https://github.com/naokiyokoyama/slurm_utils",
+    packages=setuptools.find_packages(),
+)
