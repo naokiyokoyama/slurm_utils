42023130916410336a326f05471ba61e8cbecc41
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2022-04-07 18:48:19 -0400

    sbatch path fix

--- slurm_utils/submitter.py
+++ slurm_utils/submitter.py
@@ -18,6 +18,6 @@ def submit_job(job_path, env_vars=None, other_args=None, **kwargs):
     if len(other_args_copy) > 0:
         other_args_list = [f"{k}={v}" for k, v in other_args_copy.items()]
         args += " ".join(other_args_list) + " "
-    cmd = " ".join(f"/usr/bin/sbatch {args} {job_path}".split())
+    cmd = " ".join(f"sbatch {args} {job_path}".split())
     print("[slurm_utils.submit]: Executing:", cmd)
     subprocess.check_call(cmd, shell=True)
